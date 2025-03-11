dad303d0538f400e22e1b8420ba0bc8dbffc2a22
Ahmer Ijaz <fa24-bcs-010@cuilahore.edu.pk>
2025-03-11 13:47:10 +0500

    LAB task LAB 5

---
+++ Book.java
@@ -0,0 +1,42 @@
+package LibraryManagement11March;
+
+public class Book {
+    private String title;
+    private String issn;
+    private Date publicationDate;
+    private Person author;
+
+    public Book(String title, String issn, Date publicationDate, Person author) {
+        this.title = title;
+        this.issn = issn;
+        this.publicationDate = new Date(publicationDate);
+        this.author = new Person(author);
+    }
+
+    public Book(Book other) {
+        this.title = other.title;
+        this.issn = other.issn;
+        this.publicationDate = new Date(other.publicationDate);
+        this.author = new Person(other.author);
+    }
+
+    public void showBookDetails() {
+        System.out.println("Title: " + title + ", ISSN: " + issn);
+        publicationDate.showDate();
+        author.showPersonDetails();
+    }
+
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getIssn() {
+        return issn;
+    }
+    public void setIssn(String issn) {
+        this.issn = issn;
+    }
+}
